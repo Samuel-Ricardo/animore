f364b236e1a273223d4050d2bb4ae8b82297e05b
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-21 17:46:40 -0300

    [ :art: ] | style: component - footer (app)

--- animore/src/components/footer.component.tsx
+++ animore/src/components/footer.component.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 export const Footer = () => (
-  <footer>
+  <footer className="sm:px-16 py-4 px-8 flex justify-between items-center gap-2 flex-wrap bg-[#161921]">
     <p></p>
     <Image />
     <div>
