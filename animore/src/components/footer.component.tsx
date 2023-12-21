a6066f24fd7701480387c64823f07ba7d63a5745
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-21 17:45:25 -0300

    [ :art: ] | create: component - footer (app)

---
+++ animore/src/components/footer.component.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export const Footer = () => (
+  <footer>
+    <p></p>
+    <Image />
+    <div>
+      <Image />
+      <Image />
+      <Image />
+    </div>
+  </footer>
+);
