b57dc570915101a22b3df3a2de8bf149f4066a30
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-21 17:58:54 -0300

    [:label:] | map: component - anime card (app)

--- animore/src/components/footer.component.tsx
+++ animore/src/components/footer.component.tsx
@@ -5,11 +5,35 @@ export const Footer = () => (
     <p className="text-base font-bold text-white">
       @2023 EpicAnimeVault | Samuel Ricardo
     </p>
-    <Image />
-    <div>
-      <Image />
-      <Image />
-      <Image />
+    <Image
+      src="./logo.svg"
+      alt="logo"
+      width={47}
+      height={44}
+      className="object-contain"
+    />
+    <div className="flec items-center gap-6">
+      <Image
+        src="./tiktok.svg"
+        alt="tiktok logo"
+        width={19}
+        height={19}
+        className="object-contain"
+      />
+      <Image
+        src="./instagram.svg"
+        alt="instagram logo"
+        width={19}
+        height={19}
+        className="object-contain"
+      />
+      <Image
+        src="./twitter.svg"
+        alt="twitter logo"
+        width={19}
+        height={19}
+        className="object-contain"
+      />
     </div>
   </footer>
 );
