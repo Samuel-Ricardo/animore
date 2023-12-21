1871429f6c8bba34fd265f492c4ba64d5d7d2956
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-21 17:47:32 -0300

    [ :art: ] | style: component - footer > copy (app)

--- animore/src/components/footer.component.tsx
+++ animore/src/components/footer.component.tsx
@@ -2,7 +2,9 @@ import Image from "next/image";
 
 export const Footer = () => (
   <footer className="sm:px-16 py-4 px-8 flex justify-between items-center gap-2 flex-wrap bg-[#161921]">
-    <p></p>
+    <p className="text-base font-bold text-white">
+      @2023 EpicAnimeVault | Samuel Ricardo
+    </p>
     <Image />
     <div>
       <Image />
